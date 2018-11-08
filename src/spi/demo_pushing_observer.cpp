47140005661cad41a7e99bd70ce58b3508c653bb
Lionel Ains <lionel.ains@legrand.fr>
2018-11-08 13:03:50 +0100

    Moving fake UART byte generation into demo_pushing_observer.cpp, removing raritan-specific InputObservable

--- src/spi/demo_pushing_observer.cpp
+++ src/spi/demo_pushing_observer.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
 #include <ctime>
 #include <string>
-
-#include "IAsyncDataInputObserver.h"
-#include "raritan/RaritanUartInputObservable.h"
-
 #include <sstream>
 #include <iomanip>
+#include <vector>
+#include <cstdlib> // For rand()
+#include <iostream>	// FIXME: Temp for debug
 
-using namespace std;
+#include "IAsyncDataInputObserver.h"
+#include "GenericAsyncDataInputObservable.h"
+
+class FakeUartInputObservable : public GenericAsyncDataInputObservable {
+public:
+	FakeUartInputObservable() { }
+	virtual ~FakeUartInputObservable() { }
+
+	void genRandomData() {
+		unsigned char asyncData[20];
+		size_t dataLen = static_cast<size_t>(1+static_cast<unsigned int>(rand())%sizeof(asyncData));
+		std::cout << "Observable is generating random buffer of " << dataLen << " bytes" << std::endl;
+		for (unsigned int bytePos=0; bytePos<dataLen; bytePos++) {
+			asyncData[bytePos]=static_cast<unsigned char>(rand() % 256);
+		}
+		this->notifyObservers(asyncData, dataLen);
+	}
+};
 
 class TempDisplayer : public IAsyncDataInputObserver {
 public:
-	TempDisplayer(const string& name): name(name) {};
+	TempDisplayer(const std::string& name): name(name) {};
 	~TempDisplayer() {};
 
 	virtual void handleInputData(const unsigned char* dataIn, const size_t dataLen) {
@@ -21,10 +37,10 @@ public:
 		for (size_t i =0; i<dataLen; i++) {
 			bufDump << std::hex << std::setw(2) << std::setfill('0') << static_cast<unsigned int>(dataIn[i]) << " ";
 		}
-		cout << name << ": Received buffer " << bufDump.str() << endl;
+		std::cout << name << ": Received buffer " << bufDump.str() << std::endl;
 	};
 private :
-	string name;
+	std::string name;
 };
 
 int main(int argc, char** argv) {
@@ -32,16 +48,16 @@ int main(int argc, char** argv) {
 	TempDisplayer disp1("Input processor #1");
 	TempDisplayer disp2("Input processor #2");
 
-	RaritanUartInputObservable probe;
-	probe.registerObserver(&disp1);
-	probe.registerObserver(&disp2);
+	FakeUartInputObservable inputProcessor;
+	inputProcessor.registerObserver(&disp1);
+	inputProcessor.registerObserver(&disp2);
 
 	for(int i = 0; i < 5; ++i) {
-		probe.genRandomData();
+		inputProcessor.genRandomData();
 	}
 
-	probe.unregisterObserver(&disp1);
-	probe.unregisterObserver(&disp2);
+	inputProcessor.unregisterObserver(&disp1);
+	inputProcessor.unregisterObserver(&disp2);
 
 	return 0;
 }
