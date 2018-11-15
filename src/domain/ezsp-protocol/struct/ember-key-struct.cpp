86506cc73e86fe0bdd4abafca0910fbcf9ab82ea
Sebastien MARION <sebastien.marion@legrand.fr>
2018-11-15 16:44:20 +0100

    add new structure class to decode answer

---
+++ src/domain/ezsp-protocol/struct/ember-key-struct.cpp
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+#include <sstream>
+#include <iomanip>
+
+#include "ember-key-struct.h"
+
+
+void CEmberKeyStruct::setRaw(std::vector<uint8_t> raw_message)
+{
+    bitmask = static_cast<EmberKeyStructBitmask>(raw_message.at(0) | (raw_message.at(1)<<8));
+    type = static_cast<EmberKeyType>(raw_message.at(2));
+    key.clear();
+    for(uint8_t loop=0; loop<EMBER_KEY_DATA_BYTE_SIZE; loop++)
+    {
+        key.push_back( raw_message.at(3+loop) );
+    }
+    outgoingFrameCounter = static_cast<uint32_t>(
+        raw_message.at(19) | 
+        (raw_message.at(20)<<8) |
+        (raw_message.at(21)<<16) |
+        (raw_message.at(22)<<24)
+    );        
+    incomingFrameCounter = static_cast<uint32_t>(
+        raw_message.at(23) | 
+        (raw_message.at(24)<<8) |
+        (raw_message.at(25)<<16) |
+        (raw_message.at(26)<<24)
+    );   
+    sequenceNumber = static_cast<EmberKeyType>(raw_message.at(27));
+    partnerEUI64.clear();
+    for(uint8_t loop=0; loop<EMBER_EUI64_BYTE_SIZE; loop++)
+    {
+        partnerEUI64.push_back(raw_message.at(28+loop));
+    }
+}
+
+
+std::string CEmberKeyStruct::String()
+{
+    std::stringstream buf;
+
+    buf << "EmberKeyStruct : { ";
+    buf << "[bitmask : "<< std::hex << std::setw(4) << std::setfill('0') << bitmask << "]";
+    buf << "[type : "<< CEzspEnum::EmberKeyTypeToString(type) << "]";
+    buf << "[key :";
+    for(uint8_t loop=0; loop<key.size(); loop++){ buf << " " << std::hex << std::setw(2) << std::setfill('0') << unsigned(key.at(loop)); }
+    buf << "]";
+    buf << "[outgoingFrameCounter : "<< std::hex << std::setw(8) << std::setfill('0') << outgoingFrameCounter << "]";
+    buf << "[incomingFrameCounter : "<< std::hex << std::setw(8) << std::setfill('0') << incomingFrameCounter << "]";
+    buf << "[sequenceNumber : "<< std::hex << std::setw(2) << std::setfill('0') << sequenceNumber << "]";
+    buf << "[partnerEUI64 :";
+    for(uint8_t loop=0; loop<partnerEUI64.size(); loop++){ buf << " " << std::hex << std::setw(2) << std::setfill('0') << unsigned(partnerEUI64[loop]); }
+    buf << "]";
+    buf << " }";
+
+    return buf.str();
+}
---
+++ src/domain/ezsp-protocol/struct/ember-key-struct.h
@@ -0,0 +1,67 @@
+/**
+ * A structure containing a key and its associated data.
+ */
+#pragma once
+
+#include "../ezsp-enum.h"
+
+class CEmberKeyStruct
+{
+    public:
+        CEmberKeyStruct(std::vector<uint8_t> raw_message){ setRaw(raw_message); }
+
+        // little endian encoded
+        void setRaw(std::vector<uint8_t> raw_message);
+
+        /**
+         * A bitmask indicating the presence of data within the
+         * various fields in the structure.
+         */
+        EmberKeyStructBitmask getBitmask(){ return bitmask; }
+
+        /**
+         * The type of the key.
+         */
+        EmberKeyType getType(){ return type; }
+            
+        /**
+         * The actual key data.
+         */
+        EmberKeyData getKey(){ return key; }
+
+        /**
+         * The outgoing frame counter associated with the key.
+         */
+        uint32_t getOutgoingFrameCounter(){ return outgoingFrameCounter; }
+
+        /**
+         * The frame counter of the partner device associated
+         * with the key.
+         */
+        uint32_t getIncomingFrameCounter(){ return incomingFrameCounter; }
+
+        /**
+         * The sequence number associated with the key.
+         */
+        uint8_t getSequenceNumber(){ return sequenceNumber; }
+
+        /**
+         * The IEEE address of the partner device also in
+         * possession of the key.
+         */
+        EmberEUI64 getPartnerEUI64(){ return partnerEUI64; }
+
+        /**
+         * For display
+         */
+        std::string String();
+
+    private:
+        EmberKeyStructBitmask bitmask;
+        EmberKeyType type;
+        EmberKeyData key;
+        uint32_t outgoingFrameCounter;
+        uint32_t incomingFrameCounter;
+        uint8_t sequenceNumber;
+        EmberEUI64 partnerEUI64;
+};
