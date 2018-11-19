2f1a4da85cf73d6bd460da9ea4ac2910668dfef0
Lionel Ains <lionel.ains@legrand.fr>
2018-11-19 12:37:09 +0100

    Adding write to UART in test program for Raritan SPI

--- src/spi/raritan/test_raritan.cpp
+++ src/spi/raritan/test_raritan.cpp
@@ -38,6 +38,10 @@ int main() {
 	uartDriver.setIncomingDataHandler(&uartIncomingDataHandler);
 	uartDriver.open("/dev/ttyUSB0", 57600);
 
+	unsigned char buf[5] = { 0x1a, 0xc0, 0x38, 0xbc, 0x7e};
+	size_t written;
+	uartDriver.write(written, buf, 5);
+
 	eventLoop.run();
 
 	return 0;
