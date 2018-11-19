b36816281c6753ad906350d806d5964d3d6568a5
Lionel Ains <lionel.ains@legrand.fr>
2018-11-19 18:20:05 +0100

    Lining up Makefile and applicative code to last lib interface

--- src/mainEzspTestRaritan.cpp
+++ src/mainEzspTestRaritan.cpp
@@ -1,12 +1,14 @@
 /**
+ * @file mainEzscpTestRaritan
  * 
- * 
+ * @brief Sample code for driving a dongle in the Raritan framework
  */
 
 #include <iostream>
 
 #include "spi/raritan/RaritanEventLoop.h"
 #include "spi/raritan/RaritanUartDriver.h"
+#include "spi/raritan/RaritanTimerFactory.h"
 #include "test/mainEzspTest.h"
 
 using namespace std;
@@ -16,12 +18,13 @@ int main( void )
     CAppDemo *app;
     RaritanEventLoop eventLoop;
     UartDriverRaritan uartDriver(eventLoop);
+    RaritanTimerFactory timerFactory(eventLoop);
 
     cout << "Starting ezsp test program !" << endl;
 
     uartDriver.open("/dev/ttyUSB0", 57600);
 
-    app = new CAppDemo(&uartDriver);
+    app = new CAppDemo(&uartDriver, timerFactory);
 
     eventLoop.run();
 
