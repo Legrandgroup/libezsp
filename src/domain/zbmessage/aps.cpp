13859c15f193e3fc56a58555cf8df08cf7a526b4
Martin Santowski <martin.santowski@raritan.com>
2018-12-11 14:39:27 +0100

    Use relative path for include

--- src/domain/zbmessage/aps.cpp
+++ src/domain/zbmessage/aps.cpp
@@ -1,9 +1,9 @@
 /**
- * 
+ *
  */
 
 #include "aps.h"
-#include "byte-manip.h"
+#include "../byte-manip.h"
 
 CAPSFrame::CAPSFrame() : cluster_id(0), dest_ep(0), group_id(0), option(), profile_id(0), sequence(0), src_ep(0)
 {
