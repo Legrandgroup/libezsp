64d6ff4617f659f98e76d4aa5c72ad31c791b731
Martin Santowski <martin.santowski@raritan.com>
2018-12-12 11:30:12 +0100

    remove error - unused parameter error appeared, uncomment the parameter remove this

--- src/domain/zigbee-tools/zigbee-networking.h
+++ src/domain/zigbee-tools/zigbee-networking.h
@@ -22,14 +22,14 @@ public:
     void formHaNetwork();
     void OpenNetwork( uint8_t i_timeout );
     void CloseNetwork( void );
-    
+
     void startDiscoverProduct(std::function<void (EmberNodeType i_type, EmberEUI64 i_eui64, EmberNodeId i_id)> i_discoverCallbackFct = nullptr);
 
     /**
      * Observer
      */
-    void handleDongleState( EDongleState i_state ){;}
-    void handleEzspRxMessage( EEzspCmd i_cmd, std::vector<uint8_t> i_msg_receive );    
+    void handleDongleState( EDongleState /* i_state */ ) {;}
+    void handleEzspRxMessage( EEzspCmd i_cmd, std::vector<uint8_t> i_msg_receive );
 
 private:
     CEzspDongle &dongle;
