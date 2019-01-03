62af27ed7d93ee164f2a7acd2c5be3227ec73f03
Lionel AINS <lionel.ains@legrand.fr>
2019-01-03 12:54:52 +0100

    Fixing unmerged tags

--- src/spi/GenericAsyncDataInputObservable.h
+++ src/spi/GenericAsyncDataInputObservable.h
@@ -11,15 +11,10 @@
 #include <cstdlib>	// For size_t
 #include "IAsyncDataInputObserver.h"
 
-<<<<<<< HEAD
-/**** Start of the official API; no includes below this point! ***************/
-#include <pp/official_api_start.h>
-=======
 #ifdef USE_RARITAN
 /**** Start of the official API; no includes below this point! ***************/
 #include <pp/official_api_start.h>
 #endif // USE_RARITAN
->>>>>>> master
 
 /**
  * @brief An observable class that will invoke method handleInputData() on all its observers when new async data is received
@@ -58,10 +53,6 @@ private:
 	std::set<IAsyncDataInputObserver*> observers;	/*!< The list of registered observers */
 };
 
-<<<<<<< HEAD
-#include <pp/official_api_end.h>
-=======
 #ifdef USE_RARITAN
 #include <pp/official_api_end.h>
 #endif // USE_RARITAN
->>>>>>> master
