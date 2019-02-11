3a1f06e1c45a071832c2478eff7f7f50ccf6cd45
Martin Santowski <martin.santowski@raritan.com>
2019-02-11 09:48:15 +0100

    Return error code for Uart connection open

--- src/spi/raritan/RaritanUartDriver.cpp
+++ src/spi/raritan/RaritanUartDriver.cpp
@@ -18,11 +18,15 @@ void UartDriverRaritan::setIncomingDataHandler(GenericAsyncDataInputObservable* 
 	m_data_input_observable = uartIncomingDataHandler;
 }
 
-void UartDriverRaritan::open(const std::string& serialPortName, unsigned int baudRate) {
+int UartDriverRaritan::open(const std::string& serialPortName, unsigned int baudRate) {
 	pp::Tty::UPtr tmpSerialPortUPTR;
-	pp::Tty::open(tmpSerialPortUPTR, serialPortName,
+	int err;
+	if (PP_FAILED(err = pp::Tty::open(tmpSerialPortUPTR, serialPortName,
 		pp::FileDescriptor::AF_READ_WRITE,
-		pp::FileDescriptor::CF_OPEN_EXISTING); // Should handle error here (dongle not present)
+		pp::FileDescriptor::CF_OPEN_EXISTING)))
+	{
+		return err;
+	}
 
 	m_serial_tty = std::move(tmpSerialPortUPTR);
 	m_serial_tty->setParams(static_cast<int>(baudRate), pp::Tty::Parity::None, 8, false, false, true);
@@ -31,19 +35,24 @@ void UartDriverRaritan::open(const std::string& serialPortName, unsigned int bau
 		if (events & pp::Selector::EVENT_POLLIN) {
 			unsigned char readData[256];
 			size_t rdcnt;
-			this->m_serial_tty->read(rdcnt, readData, sizeof(readData)/sizeof(unsigned char));
+			int error;
+			if (PP_FAILED(error = this->m_serial_tty->read(rdcnt, readData, sizeof(readData)/sizeof(unsigned char)))){
+				PPD_ERR_VAL(error, "Tty.read()");
+				return;
+			}
 			PPD_DEBUG_HEX("read from dongle: ", readData, rdcnt);
 			if (this->m_data_input_observable)
 				this->m_data_input_observable->notifyObservers(readData, rdcnt);
 			//this->m_eventLoop.getSelector().stopAsync();
 		}
 	};
-	this->m_eventLoop.getSelector().addSelectable(m_sel_handle, m_serial_tty, POLLIN, cbin);
+	m_eventLoop.getSelector().addSelectable(m_sel_handle, m_serial_tty, POLLIN, cbin);
+	return PP_OK;
 }
 
 int UartDriverRaritan::write(size_t& writtenCnt, const void* buf, size_t cnt) {
 	PPD_DEBUG_HEX("write to dongle: ", buf, cnt);
-	int result = this->m_serial_tty->write(writtenCnt, buf, cnt);
+	int result = m_serial_tty->write(writtenCnt, buf, cnt);
 	if (result == PP_OK) {
 		//PPD_DEBUG("Successfully wrote %d bytes", cnt);
 		return 0;
