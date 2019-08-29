c20c17e96dc14d5f04f40a2219122b65e7f263f4
Lionel AINS <lionel.ains@legrand.fr>
2019-08-29 08:46:04 +0200

    Adding template for gp_recv_sensor_measurement unit test (writing framework for lambda on write callback)

--- src/tests/gp_tests.cpp
+++ src/tests/gp_tests.cpp
@@ -1,7 +1,18 @@
 #include "TestHarness.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
+#include <iostream>
+
+#include "../spi/mock-uart/MockUartDriver.h"
+
+class GPRecvSensorMeasurementTest {
+public:
+	GPRecvSensorMeasurementTest() : step(0) { }
+	int onWriteCallback(size_t& writtenCnt, const void* buf, size_t cnt, std::chrono::duration<double, std::milli> delta) {
+		std::cout << "Request to write " << writtenCnt << " bytes: " << /*hexdump(buf, cnt) << */ "delta since last write: " << 0 /*delta*/ << "ms)\n";
+		return 0;
+	}
+private:
+	unsigned int step;
+};
 
 TEST_GROUP(gp_tests) {
 };
@@ -13,20 +24,23 @@ TEST(gp_tests, dummy_test) {
 	NOTIFYPASS();
 }
 
-#if 0
-TEST(node_tests, dummy_optional_test) {
-	const char* expected_result=NULL;
-
-	if (result != NULL) != 0)
-		FAILF("Test failed, got:\n\"%p\", expected:\n\"%s\"\n", result, "NULL");
+TEST(gp_tests, gp_recv_sensor_measurement) {
+	GPRecvSensorMeasurementTest testContext;
+	auto cb = [&testContext](size_t& writtenCnt, const void* buf, size_t cnt, std::chrono::duration<double, std::milli> delta) -> int {
+		return testContext.onWriteCallback(writtenCnt, buf, cnt, delta);
+	};
+	MockUartDriver uartDriver(cb);
+    if (uartDriver.open("/dev/ttyUSB0", 57600) != 0) {
+        std::cerr << "Failed opening serial port. Aborting\n";
+    }
 
 	NOTIFYPASS();
 }
-#endif
 
 
 #ifndef USE_CPPUTEST
 void unit_tests_gp() {
 	dummy_test();
+	gp_recv_sensor_measurement();
 }
 #endif	// USE_CPPUTEST
