6463e4f860995f08091b516826d4644d48520326
Lionel AINS <lionel.ains@legrand.fr>
2019-08-29 09:09:28 +0200

    Adding mock serial self unit tests

---
+++ src/tests/mock_serial_self_tests.cpp
@@ -0,0 +1,38 @@
+#include "TestHarness.h"
+#include <iostream>
+
+#include "../spi/mock-uart/MockUartDriver.h"
+
+class GenericSerialProcessor {
+public:
+	GenericSerialProcessor() : step(0) { }
+	int onWriteCallback(size_t& writtenCnt, const void* buf, size_t cnt, std::chrono::duration<double, std::milli> delta) {
+		std::cout << "Request to write " << writtenCnt << " bytes: " << /*hexdump(buf, cnt) << */ "delta since last write: " << 0 /*delta*/ << "ms)\n";
+		return 0;
+	}
+private:
+	unsigned int step;
+};
+
+TEST_GROUP(mock_serial_tests) {
+};
+
+TEST(mock_serial_tests, mock_serial_open) {
+	GenericSerialProcessor serialProcessor;
+	auto cb = [&serialProcessor](size_t& writtenCnt, const void* buf, size_t cnt, std::chrono::duration<double, std::milli> delta) -> int {
+		return serialProcessor.onWriteCallback(writtenCnt, buf, cnt, delta);
+	};
+	MockUartDriver uartDriver(cb);
+    if (uartDriver.open("/dev/ttyUSB0", 57600) != 0) {
+        FAILF("Failed opening mock serial port");
+    }
+    FAILF("Failure");
+	NOTIFYPASS();
+}
+
+
+#ifndef USE_CPPUTEST
+void unit_tests_mock_serial() {
+	mock_serial_open();
+}
+#endif	// USE_CPPUTEST
--- src/tests/test_libezsp.cpp
+++ src/tests/test_libezsp.cpp
@@ -12,11 +12,14 @@
 
 #ifndef USE_CPPUTEST
 void unit_tests_gp();	// Declaration of gp unit test procedure (see gp_tests.cpp)
+void unit_tests_mock_serial();	// Declaration of mock serial self tests (see mock_serial_self_tests.cpp)
 #endif
 
 int main(int argc, char* argv[]) {
 
 #ifndef USE_CPPUTEST
+	printf("*** Self testi on mock serial ***\n");
+	unit_tests_mock_serial();
 	printf("*** Testing GP frames processing ***\n");
 	unit_tests_gp();
 	printf("\n*** All unit tests passed successfully ***\n");
