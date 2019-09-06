e471a4728b3c5279b8a53a3dec4a32d0c1080081
Lionel AINS <lionel.ains@legrand.fr>
2019-09-06 10:17:06 +0200

    Implementing 2 more steps in gp_recv_sensor_measurement unit test

--- src/tests/gp_tests.cpp
+++ src/tests/gp_tests.cpp
@@ -33,7 +33,7 @@ public:
 				FAILF("First call to write() on mock serial interface should not lead to a valid delta\n");
 			}
 		}
-		std::cout << "\n";
+		std::cout << " (current stage " << std::dec << this->stage << ")\n";
 		writtenCnt = cnt;
 		if (this->stage == 0 && compareBufWithVector(buf, cnt, std::vector<uint8_t>({0x1a, 0xc0, 0x38, 0xbc, 0x7e}))) {
 			std::cout << "Got a ASH reset command\n";
@@ -42,6 +42,22 @@ public:
 		else if (this->stage == 1 && compareBufWithVector(buf, cnt, std::vector<uint8_t>({0x00, 0x42, 0x21, 0xa8, 0x52, 0xcd, 0x6e, 0x7e}))) {
 			std::cout << "Got a ASH get stack version command\n";
 			this->stage++;
+		}
+		else if (this->stage == 2 && compareBufWithVector(buf, cnt, std::vector<uint8_t>({0x81, 0x60, 0x59, 0x7e}))) {
+			std::cout << "Got a ASH command at stage 3\n";
+			this->stage++;
+		}
+		else if (this->stage == 3 && compareBufWithVector(buf, cnt, std::vector<uint8_t>({0x7d, 0x31, 0x43, 0x21, 0xa8, 0x53, 0x05, 0xf0, 0x7e}))) {
+			std::cout << "Got a ASH command at stage 4\n";
+			this->stage++;
+		}
+		else {
+			std::cerr << "Warning: Got an unexpected command written to serial port while at stage " << std::dec << this->stage << ":";
+			for (uint8_t loop=0; loop<cnt; loop++) {
+				std::cerr << " " << std::hex << std::setw(2) << std::setfill('0') << unsigned((static_cast<const unsigned char*>(buf))[loop]);
+			}
+			std::cerr << "\n";
+			FAILF("Unexpected command written to serial port");
 		}
 		this->nbWriteCalls++;
 		return 0;
@@ -124,10 +140,11 @@ TEST(gp_tests, gp_recv_sensor_measurement) {
 	else
 		std::cout << "ASH get stack version confirmed\n";
 
+	std::cerr << "Remaining inncoming queue:" << uartDriver.scheduledIncomingChunksToString() << "\n";
 	{
 		MockUartScheduledByteDelivery rBuf;
 		rBuf.delay = std::chrono::seconds(0);  /* Make bytes available now */
-		rBuf.byteBuffer = std::vector<uint8_t>({0x1a, 0xc1, 0x02, 0x0b, 0x0a, 0x52, 0x7e});
+		rBuf.byteBuffer = std::vector<uint8_t>({0x01, 0x42, 0xa1, 0xa8, 0x53, 0x28, 0x45, 0xd7, 0xcf, 0x00, 0x7e});
 		uartDriver.scheduleIncomingChunk(rBuf);
 	}
 
