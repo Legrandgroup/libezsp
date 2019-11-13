dd66dcbc62e7748e6a69e81cd468c3e61d258b7a
seb <sebastien.marion@legrand.fr>
2019-11-13 15:47:47 +0100

    adding class to manage ember gp sink table entry structure

--- src/domain/ezsp-protocol/struct/ember-gp-sink-table-entry-struct.cpp
+++ src/domain/ezsp-protocol/struct/ember-gp-sink-table-entry-struct.cpp
@@ -11,7 +11,102 @@
 
 #include "../../byte-manip.h"
 
-CEmberGpSinkTableEntryStruct::CEmberGpSinkTableEntryStruct(const std::vector<uint8_t>& raw_message)
+CEmberGpSinkTableEntryStruct::CEmberGpSinkTableEntryStruct(const std::vector<uint8_t>& raw_message):
+        status(raw_message.at(0)),
+        options(dble_u8_to_u16(raw_message.at(1),raw_message.at(2))),
+        gpd(std::vector<uint8_t>(raw_message.begin()+3,raw_message.end())),
+        device_id(raw_message.at(13)),
+        sink_list(),
+        assigned_alias(dble_u8_to_u16(raw_message.at(36),raw_message.at(37))),
+        groupcast_radius(raw_message.at(38)),
+        security_options(raw_message.at(39)),
+        gpdSecurity_frame_counter(static_cast<EmberGpSecurityFrameCounter>(quad_u8_to_u32(raw_message.at(40),raw_message.at(41),raw_message.at(42),raw_message.at(43)))),
+        gpd_key(raw_message.begin()+44,raw_message.begin()+60)
 {
+    sink_list[0].push_back(0xFF);
+    sink_list[1].push_back(0xFF);
+}
 
+/**
+ * @brief constructor with specific value, others are set to default value.
+ *          option : 0x02A8 (cf. A3.3.2.2.1 Options parameter of the Sink Table from doc doc-14-0563-16-batt-green-power-spec_ProxyBasic.pdf)
+ *              - bits 0..2 : Application Id (0b000 : use source Id)
+ *              - bits 3..4 : Communication mode (0b01 : groupcast forwarding of the GP Notification command to DGroupID)
+ *              - bit 5 : Sequence number capabilities (0b1 : use incremental sequence number)
+ *              - bit 6 : RxOnCapability (0b0 : not capable)
+ *              - bit 7 : FixedLocation (0b1 : not a mobile device)
+ *              - bit 8 : AssignedAlias (0b0 : the derived alias is used)
+ *              - bit 9 : Security use (0b1 : indicates that security-related parameters of the Sink Table entry are present)
+ *              - bit 10..15 : Reserved
+ *          sink_list : by reverse set first byte of each to 0xFF to disable usage.
+ *          groupcast_radius : The default value of 0x00 indicates undefined
+ * 
+ * @param i_status Internal status of the sink table entry. 0x01 active, 0xff : disable
+ * @param i_gpd_address The addressing info of the GPD.
+ * @param i_device_id The device id for the GPD.
+ * @param i_alias The assigned alias for the GPD.
+ * @param i_security_option The security options field. currently 0x12 : SecurtityLevel (0b10), SecurityKeyType(0b100)
+ * @param i_frm_counter the last observed valid frame counter value
+ * @param i_gpd_key The key to use for GPD.
+ */
+CEmberGpSinkTableEntryStruct::CEmberGpSinkTableEntryStruct(EmberGpSinkTableEntryStatus i_status, 
+                CEmberGpAddressStruct i_gpd_address, uint8_t i_device_id, uint16_t i_alias,
+                uint8_t i_security_option, EmberGpSecurityFrameCounter i_frm_counter, EmberKeyData i_gpd_key):
+        status(i_status),
+        options(0x02A8),
+        gpd(i_gpd_address),
+        device_id(i_device_id),
+        sink_list(),
+        assigned_alias(i_alias),
+        groupcast_radius(0),
+        security_options(i_security_option),
+        gpdSecurity_frame_counter(i_frm_counter),
+        gpd_key(i_gpd_key)
+{
+    sink_list[0].push_back(0xFF);
+    sink_list[1].push_back(0xFF);
+}
+
+
+/**
+ * @brief return structure as a raw
+ * 
+ * @return raw of structure
+ */
+std::vector<uint8_t> CEmberGpSinkTableEntryStruct::getRaw(void)
+{
+    std::vector<uint8_t> l_struct;
+
+    // Internal status of the sink table entry.
+    l_struct.push_back(status); // 0x01 : active, 0xff : disable
+    // The tunneling options (this contains both options and extendedOptions from the spec). WARNING 16 bits !!!
+    l_struct.push_back(u16_get_lo_u8(options));
+    l_struct.push_back(u16_get_hi_u8(options));
+    // The addressing info of the GPD.
+    std::vector<uint8_t> l_addr = gpd.getRaw();
+    l_struct.insert(l_struct.end(), l_addr.begin(), l_addr.end());
+    // The device id for the GPD.
+    l_struct.push_back(device_id);
+    // The list of sinks (hardcoded to 2 which is the spec minimum).
+    for( int loop=0; loop<GP_SINK_LIST_ENTRIES; loop++ )
+    {
+        l_struct.insert(l_struct.end(), sink_list[loop].begin(), sink_list[loop].end());
+    }
+    // The assigned alias for the GPD.
+    l_struct.push_back(u16_get_lo_u8(assigned_alias));
+    l_struct.push_back(u16_get_hi_u8(assigned_alias));
+    // The groupcast radius.
+    l_struct.push_back(groupcast_radius);
+    // The security options field.
+    l_struct.push_back(security_options);
+    // The security frame counter of the GPD.
+    l_struct.push_back(static_cast<uint8_t>(gpdSecurity_frame_counter&0xFF));
+    l_struct.push_back(static_cast<uint8_t>((gpdSecurity_frame_counter>>8)&0xFF));
+    l_struct.push_back(static_cast<uint8_t>((gpdSecurity_frame_counter>>16)&0xFF));
+    l_struct.push_back(static_cast<uint8_t>((gpdSecurity_frame_counter>>24)&0xFF));
+
+    // The key to use for GPD.
+    l_struct.insert(l_struct.end(), gpd_key.begin(), gpd_key.end());
+
+    return l_struct;
 }
--- src/domain/ezsp-protocol/struct/ember-gp-sink-table-entry-struct.h
+++ src/domain/ezsp-protocol/struct/ember-gp-sink-table-entry-struct.h
@@ -7,6 +7,7 @@
 #pragma once
 
 #include "../ezsp-enum.h"
+#include "ember-gp-address-struct.h"
 
 #ifdef USE_RARITAN
 /**** Start of the official API; no includes below this point! ***************/
@@ -31,6 +32,32 @@ class CEmberGpSinkTableEntryStruct
         CEmberGpSinkTableEntryStruct(const std::vector<uint8_t>& raw_message);
 
         /**
+         * @brief constructor with specific value, others are set to default value.
+         *          option : 0x02A8 (cf. A3.3.2.2.1 Options parameter of the Sink Table from doc doc-14-0563-16-batt-green-power-spec_ProxyBasic.pdf)
+         *              - bits 0..2 : Application Id (0b000 : use source Id)
+         *              - bits 3..4 : Communication mode (0b01 : groupcast forwarding of the GP Notification command to DGroupID)
+         *              - bit 5 : Sequence number capabilities (0b1 : use incremental sequence number)
+         *              - bit 6 : RxOnCapability (0b0 : not capable)
+         *              - bit 7 : FixedLocation (0b1 : not a mobile device)
+         *              - bit 8 : AssignedAlias (0b0 : the derived alias is used)
+         *              - bit 9 : Security use (0b1 : indicates that security-related parameters of the Sink Table entry are present)
+         *              - bit 10..15 : Reserved
+         *          sink_list : by reverse set first byte of each to 0xFF to disable usage.
+         *          groupcast_radius : The default value of 0x00 indicates undefined
+         * 
+         * @param i_status Internal status of the sink table entry. 0x01 active, 0xff : disable
+         * @param i_gpd_address The addressing info of the GPD.
+         * @param i_device_id The device id for the GPD.
+         * @param i_alias The assigned alias for the GPD.
+         * @param i_security_option The security options field. currently 0x12 : SecurtityLevel (0b10), SecurityKeyType(0b100)
+         * @param i_frm_counter the last observed valid frame counter value
+         * @param i_gpd_key The key to use for GPD.
+         */
+        CEmberGpSinkTableEntryStruct(EmberGpSinkTableEntryStatus i_status, 
+                        CEmberGpAddressStruct i_gpd_address, uint8_t i_device_id, uint16_t i_alias,
+                        uint8_t i_security_option, EmberGpSecurityFrameCounter i_frm_counter, EmberKeyData i_gpd_key);        
+
+        /**
          * @brief Copy constructor
          *
          * Copy construction is forbidden on this class
@@ -44,10 +71,35 @@ class CEmberGpSinkTableEntryStruct
          */
         CEmberGpSinkTableEntryStruct& operator=(const CEmberGpSinkTableEntryStruct& other) = delete;
 
+        /**
+         * @brief return structure as a raw
+         * 
+         * @return raw of structure
+         */
+        std::vector<uint8_t> getRaw(void);
+
+
     private:
-
-
-
+        // Internal status of the sink table entry.
+        EmberGpSinkTableEntryStatus status;
+        // The tunneling options (this contains both options and extendedOptions from the spec). WRONG Specification only 16 bits like option without extended ...
+        uint16_t options;
+        // The addressing info of the GPD.
+        CEmberGpAddressStruct gpd;
+        // The device id for the GPD.
+        uint8_t device_id;
+        // The list of sinks (hardcoded to 2 which is the spec minimum).
+        EmberGpSinkListEntry sink_list[GP_SINK_LIST_ENTRIES];
+        // The assigned alias for the GPD.
+        EmberNodeId assigned_alias;
+        // The groupcast radius.
+        uint8_t groupcast_radius;
+        // The security options field.
+        uint8_t security_options;
+        // The security frame counter of the GPD.
+        EmberGpSecurityFrameCounter gpdSecurity_frame_counter;
+        // The key to use for GPD.
+        EmberKeyData gpd_key;
 };
 
 #ifdef USE_RARITAN
