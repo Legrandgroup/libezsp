323de9ced1e47da28a7b715b208b006ec68fdffa
seb <sebastien.marion@legrand.fr>
2019-11-21 14:46:00 +0100

    register an hardcoded gpd if we do not request to open gpd commissioning for debug

--- src/example/CAppDemo.cpp
+++ src/example/CAppDemo.cpp
@@ -280,6 +280,12 @@ void CAppDemo::handleEzspRxMessage( EEzspCmd i_cmd, std::vector<uint8_t> i_msg_r
                     // If requested to do so, immediately open a GP commissioning session
                     gp_sink.openCommissioningSession();
                 }
+                else
+                {
+                    // debug : add hardcoded gpd
+                    gp_sink.registerGpd(0x00510050,{0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F});
+                }
+                
 
                 if (this->openZigbeeCommissionningAtStartup) {
                     // If requested to do so, open the zigbee network for a specific duration, so new devices can join
