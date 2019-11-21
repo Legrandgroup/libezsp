bbe6c290d068ca88e13692effa548e885d90b9d2
seb <sebastien.marion@legrand.fr>
2019-11-21 12:47:37 +0100

    fill class

--- src/domain/ezsp-protocol/struct/ember-process-gp-pairing-parameter.cpp
+++ src/domain/ezsp-protocol/struct/ember-process-gp-pairing-parameter.cpp
@@ -5,3 +5,63 @@
 
 #include "ember-process-gp-pairing-parameter.h"
 
+
+
+CProcessGpPairingParam::CProcessGpPairingParam(CEmberGpSinkTableEntryStruct i_sink_table_entry, bool i_add_sink, bool i_remove_gpd, 
+                                                uint16_t i_sinkNetworkAddress, EmberEUI64 i_sinkIeeeAddress ) :
+        options(i_sink_table_entry.getOption(), i_add_sink, i_remove_gpd, i_sink_table_entry.getSecurityLevel(), i_sink_table_entry.getSecurityKeyType(), true, true, true),
+        addr(i_sink_table_entry.getGpdAddr()),
+        commMode(i_sink_table_entry.getOption().getCommunicationMode()),
+        sinkNetworkAddress(i_sinkNetworkAddress),
+        sinkGroupId(static_cast<uint16_t>(i_sink_table_entry.getGpdAddr().getSourceId()&0xFFFF)),
+        assignedAlias(i_sink_table_entry.getAssignedAlias()),
+        sinkIeeeAddress(i_sinkIeeeAddress),
+        gpdKey(i_sink_table_entry.getGpdKey()),
+        gpdSecurityFrameCounter(i_sink_table_entry.getSecurityFrameCounter()),
+        forwardingRadius(i_sink_table_entry.getGroupcastRadius())
+{
+
+}
+
+
+/**
+ * @brief raw getter
+ */
+std::vector<uint8_t> CProcessGpPairingParam::get(void)
+{
+    std::vector<uint8_t> lo_out;
+
+    // The options field of the GP Pairing command
+    uint32_t l_option = options.get();
+    lo_out.push_back(static_cast<uint8_t>(l_option&0xFF));
+    lo_out.push_back(static_cast<uint8_t>((l_option>>8)&0xFF));
+    lo_out.push_back(static_cast<uint8_t>((l_option>>16)&0xFF));
+    lo_out.push_back(static_cast<uint8_t>((l_option>>24)&0xFF));
+    // The addressing info of the target GPD.
+    std::vector<uint8_t> l_gpd_addr = addr.getRaw();
+    lo_out.insert(lo_out.end(),l_gpd_addr.begin(),l_gpd_addr.end());
+    // The communication mode of the GP Sink.
+    lo_out.push_back(commMode);
+    // The network address of the GP Sink.
+    lo_out.push_back(static_cast<uint8_t>(sinkNetworkAddress&0xFF));
+    lo_out.push_back(static_cast<uint8_t>((sinkNetworkAddress>>8)&0xFF));
+    // The group ID of the GP Sink.
+    lo_out.push_back(static_cast<uint8_t>(sinkGroupId&0xFF));
+    lo_out.push_back(static_cast<uint8_t>((sinkGroupId>>8)&0xFF));
+    // The alias assigned to the GPD.
+    lo_out.push_back(static_cast<uint8_t>(assignedAlias&0xFF));
+    lo_out.push_back(static_cast<uint8_t>((assignedAlias>>8)&0xFF));
+    // The IEEE address of the GP Sink.
+    lo_out.insert(lo_out.end(),sinkIeeeAddress.begin(),sinkIeeeAddress.end());
+    // The key to use for GPD.
+    lo_out.insert(lo_out.end(),gpdKey.begin(),gpdKey.end());
+    // The security frame counter of the GPD.
+    lo_out.push_back(static_cast<uint8_t>(gpdSecurityFrameCounter&0xFF));
+    lo_out.push_back(static_cast<uint8_t>((gpdSecurityFrameCounter>>8)&0xFF));
+    lo_out.push_back(static_cast<uint8_t>((gpdSecurityFrameCounter>>16)&0xFF));
+    lo_out.push_back(static_cast<uint8_t>((gpdSecurityFrameCounter>>24)&0xFF));
+    // The forwarding radius.
+    lo_out.push_back(forwardingRadius);
+
+    return lo_out;
+}
--- src/domain/ezsp-protocol/struct/ember-process-gp-pairing-parameter.h
+++ src/domain/ezsp-protocol/struct/ember-process-gp-pairing-parameter.h
@@ -7,6 +7,8 @@
 #include "../../byte-manip.h"
 #include "../ezsp-enum.h"
 #include "ember-gp-address-struct.h"
+#include "ember-gp-sink-table-entry-struct.h"
+#include "../../zbmessage/gp-pairing-command-option-struct.h"
 
 
 #ifdef USE_RARITAN
@@ -31,13 +33,31 @@ class CProcessGpPairingParam
          */
         CProcessGpPairingParam(const CProcessGpPairingParam& other) = delete;
 
+        /**
+         * @brief constructor using sink table entry and extra parameter
+         * 
+         * @param i_sink_table_entry : sink table entry corresponding to gpd
+         * @param i_add_sink : true to add, false to remove
+         * @param i_remove_gpd : true to remove
+         * @param i_sinkNetworkAddress : short network address of sink
+         * @param i_sinkIeeeAddress : long network address of sink
+         */
+        CProcessGpPairingParam(CEmberGpSinkTableEntryStruct i_sink_table_entry, bool i_add_sink, bool i_remove_gpd, 
+                                uint16_t i_sinkNetworkAddress, EmberEUI64 i_sinkIeeeAddress );
+
+
+        /**
+         * @brief raw getter
+         */
+        std::vector<uint8_t> get(void);
+
 
     private:
         /**
          * The options field of the GP Pairing command.
          * see A.3.3.5.2 GP Pairing command for more details
          */
-        uint32_t options;
+        CGpPairingCommandOption options;
         /**
          * The target GPD.
          */
