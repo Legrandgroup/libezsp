025f96d8347347dd3bd20b2ae6e5b939fecfb697
seb <sebastien.marion@legrand.fr>
2019-11-22 14:04:08 +0100

    adding optional field for register a gpd

--- src/domain/zbmessage/green-power-device.cpp
+++ src/domain/zbmessage/green-power-device.cpp
@@ -8,8 +8,31 @@
 
 CGpDevice::CGpDevice(uint32_t i_source_id, const EmberKeyData& i_key) :
     source_id(i_source_id),
-	key(i_key)
+	key(i_key),
+	option(0x02A8),
+	security_option(0x12)
 {
+}
+
+/**
+ * This method is a friend of CEmberGpSinkTableOption class
+ * swap() is needed within operator=() to implement to copy and swap paradigm
+**/
+void swap(CGpDevice& first, CGpDevice& second) /* nothrow */
+{
+  using std::swap;	// Enable ADL
+
+  swap(first.source_id, second.source_id);
+  swap(first.key, second.key);
+  swap(first.option, second.option);
+  swap(first.security_option, second.security_option);
+  /* Once we have swapped the members of the two instances... the two instances have actually been swapped */
+}
+
+CGpDevice& CGpDevice::operator=( CGpDevice other)
+{
+  swap(*this, other);
+  return *this;
 }
 
 uint32_t CGpDevice::getSourceId() const
--- src/domain/zbmessage/green-power-device.h
+++ src/domain/zbmessage/green-power-device.h
@@ -8,6 +8,7 @@
 
 #include <cstdint>
 #include "../ezsp-protocol/ezsp-enum.h"
+#include "../ezsp-protocol/struct/ember-gp-sink-table-options-field.h"
 
 /**
  * @brief Class to encapsulate data representing a green power device
@@ -34,9 +35,19 @@ class CGpDevice
         /**
          * @brief Assignment operator
          *
-         * Copy construction is forbidden on this class
          */
-        CGpDevice& operator=(const CGpDevice& other) = delete;
+        CGpDevice& operator=(const CGpDevice other);
+
+        /**
+         * @brief swap function to allow implementing of copy-and-swap idiom on members of type CEmberGpSinkTableOption
+         *
+         * This function will swap all attributes of \p first and \p second
+         * See http://stackoverflow.com/questions/3279543/what-is-the-copy-and-swap-idiom
+         *
+         * @param first The first object
+         * @param second The second object
+         */
+        friend void (::swap)(CGpDevice& first, CGpDevice& second); 
 
         /**
          * @brief Retrieve the source id for this device
@@ -52,7 +63,23 @@ class CGpDevice
          */
         EmberKeyData getKey() const;
 
+        /**
+         * @brief Retrieve the sink option table for this device
+         *
+         * @return The option
+         */
+        CEmberGpSinkTableOption getSinkOption() const { return option; }
+
+        /**
+         * @brief Retrieve the sink security option table for this device
+         *
+         * @return The security option
+         */
+        uint8_t getSinkSecurityOption() const { return security_option; }
+
     private:
         uint32_t     source_id;	/*!< The source ID for this device */
         EmberKeyData key; /*!< The key for this device */
+        CEmberGpSinkTableOption option; /*!< Sink table option for this device */
+        uint8_t security_option; /*!< Sink table security option for this device */
 };
