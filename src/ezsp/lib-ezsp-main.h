ed5706ac038092d4561bec8fd615e27c9b954dca
Lionel AINS <lionel.ains@legrand.fr>
2019-12-19 15:26:09 +0100

    Adding typedef for callback functions and lining-up their naming

--- src/ezsp/lib-ezsp-main.h
+++ src/ezsp/lib-ezsp-main.h
@@ -36,7 +36,10 @@ enum class CLibEzspState {
 class CLibEzspMain : public CEzspDongleObserver, CGpObserver
 {
 public:
-    typedef std::function<void (uint32_t &i_gpd_id, bool i_gpd_known, CGpdKeyStatus i_gpd_key_status)> FGpdSourceIdCallback;
+    typedef std::function<void (CLibEzspState& i_state)> FStateCallback;    /*!< Callback type for method registerLibraryStateCallback() */
+    typedef std::function<void (CGpFrame &i_gpf)> FGpFrameRecvCallback; /*!< Callback type for method registerGPFrameRecvCallback() */
+    typedef std::function<void (uint32_t &i_gpd_id, bool i_gpd_known, CGpdKeyStatus i_gpd_key_status)> FGpSourceIdCallback; /*!< Callback type for method registerGPSourceIdCallback() */
+
     /**
      * @brief Default constructor with minimal args to initialize library
      *
@@ -54,21 +57,21 @@ public:
      *
      * @param newObsStateCallback A callback function of type void func(CLibEzspState& i_state), that will be invoked each time our internal state will change (or nullptr to disable callbacks)
      */
-    void registerLibraryStateCallback(std::function<void (CLibEzspState& i_state)> newObsStateCallback);
+    void registerLibraryStateCallback(FStateCallback newObsStateCallback);
 
     /**
-     * @brief Register callback to receive all incoming greenpower sourceId
+     * @brief Register callback to receive all authenticated incoming green power frames
      *
-     * @param newObsGPFrameRecvCallback A callback function of type void func(CGpFrame &i_gpf), that will be invoked each time a new valid green power frame is received from a known source ID (or nullptr to disable callbacks)
+     * @param newObsGPFrameRecvCallback A callback function that will be invoked each time a new valid green power frame is received from a known source ID (or nullptr to disable callbacks)
      */
-    void registerGPFrameRecvCallback(std::function<void (CGpFrame &i_gpf)> newObsGPFrameRecvCallback);
+    void registerGPFrameRecvCallback(FGpFrameRecvCallback newObsGPFrameRecvCallback);
 
     /**
-     * @brief Register callback to receive all incoming greenpower sourceId
+     * @brief Register callback to receive all incoming green power sourceId
      *
-     * @param newObsGPSourceIdCallback A callback function of type void func(uint32_t &i_gpd_id, bool i_gpd_known, CGpdKeyStatus i_gpd_key_status), that will be invoked each time a new source ID transmits over the air (or nullptr to disable callbacks)
+     * @param newObsGPSourceIdCallback A callback function that will be invoked each time a new source ID transmits over the air (or nullptr to disable callbacks)
      */
-    void registerGPSourceIdCallback(FGpdSourceIdCallback newObsGPSourceIdCallback);
+    void registerGPSourceIdCallback(FGpSourceIdCallback newObsGPSourceIdCallback);
 
     /**
      * @brief Remove GP all devices from sink
@@ -112,13 +115,13 @@ private:
     TimerBuilder &timerbuilder;	/*!< A builder to create timer instances */
     uint8_t exp_ezsp_version;   /*!< Expected EZSP version from dongle, at initial state then current version of dongle */
     CLibEzspState lib_state;    /*!< Current state for our internal state machine */
-    std::function<void (CLibEzspState& i_state)> obsStateCallback;	/*!< Optional user callback invoked by us each time library state change */
+    FStateCallback obsStateCallback;	/*!< Optional user callback invoked by us each time library state change */
     CEzspDongle dongle; /*!< Dongle manipulation handler */
     CZigbeeMessaging zb_messaging;  /*!< Zigbee messages utility */
     CZigbeeNetworking zb_nwk;   /*!< Zigbee networking utility */
     CGpSink gp_sink;    /*!< Internal Green Power sink utility */
-    std::function<void (CGpFrame &i_gpf)> obsGPFrameRecvCallback;   /*!< Optional user callback invoked by us each time a green power message is received */
-    FGpdSourceIdCallback obsGPSourceIdCallback;	/*!< Optional user callback invoked by us each time a green power message is received */
+    FGpFrameRecvCallback obsGPFrameRecvCallback;   /*!< Optional user callback invoked by us each time a green power message is received */
+    FGpSourceIdCallback obsGPSourceIdCallback;	/*!< Optional user callback invoked by us each time a green power message is received */
 
     void setState( CLibEzspState i_new_state );
     CLibEzspState getState() const;
