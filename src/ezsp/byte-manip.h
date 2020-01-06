5ea7009e931c6439f9e611d965dc8a54acc9ae39
mchalain <marc.chalain@smile.fr>
2020-01-06 11:32:28 +0100

    [ezsp] add byte manip inside NSEZSP namespace

--- src/ezsp/byte-manip.h
+++ src/ezsp/byte-manip.h
@@ -6,6 +6,8 @@
 
 #include <cstdint>
 
+namespace NSEZSP {
+
 inline uint16_t dble_u8_to_u16(const uint8_t highByte, const uint8_t lowByte) {
 	uint16_t value = static_cast<uint16_t>(highByte) << 8;
 	value |= static_cast<uint16_t>(lowByte);
@@ -27,3 +29,5 @@ inline uint8_t u16_get_hi_u8(const uint16_t word) {
 inline uint8_t u16_get_lo_u8(const uint16_t word) {
 	return static_cast<uint8_t>(word & 0xFF);
 }
+
+} // namespace NSEZSP
