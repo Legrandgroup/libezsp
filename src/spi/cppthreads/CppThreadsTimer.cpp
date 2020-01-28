Legrandgroup/libezsp

commit a1562b30b2774acc7368ab69ab3dcea5b40613df

[spi] CppThreadTimer remove useless Lambda

src/spi/cppthreads/CppThreadsTimer.cpp
@@ -67,7 +67,7 @@ bool CppThreadsTimer::isRunning() {
 void CppThreadsTimer::routine()
 {
 	std::unique_lock<std::mutex> lock(this->cv_m);
-	this->cv.wait_for(lock, std::chrono::milliseconds(this->duration), [this]{return !this->started;});
+	this->cv.wait_for(lock, std::chrono::milliseconds(this->duration));
 	if (this->started) {
 		this->callback(this);
 	}

include/spi/ITimer.h
@@ -26,6 +26,8 @@ protected:
 	virtual void trigger(ITimer* triggeringTimer) = 0;
 };
 
+typedef std::function<void (ITimer* triggeringTimer)> TimerCallback;
+
 /**
  * @brief Abstract class to execute a callback after a given timeout
  */
@@ -47,7 +49,7 @@ public:
 	 * @param timeout The timeout (in ms)
 	 * @param callBackFunction The function to call at expiration of the timer (should be of type void f(ITimer*)) where argument will be a pointer to this timer object that invoked the callback
 	 */
-	virtual bool start(uint16_t timeout, std::function<void (ITimer* triggeringTimer)> callBackFunction) = 0;
+	virtual bool start(uint16_t timeout, TimerCallback callBackFunction) = 0;
 
 	/**
 	 * @brief Start a timer, run a callback after expiration of the configured time

src/spi/cppthreads/CppThreadsTimer.h
@@ -12,8 +12,6 @@
 #include <condition_variable>
 namespace NSSPI {
 
-typedef std::function<void (ITimer* triggeringTimer)> TimerCallback;
-
 /**
  * @brief Concrete implementation of ITimer using C++11 threads
  */

commit e3746d175ca093d57c755b795e637f5d1d20dece

Fixing spurious wake-up detection on CppThreadsTimer
Without this fix, when in bootloader mode, timer threads would not be interrupted properly when calling stop()
They would thus wait until expiration (and then not run the trigger function), this causes the UART observer notification to last as long and the stop() function, thus as long as the timeout
This, in turn means we read the serial port only at a period equal to the timeout, thus becoming very slow
This bug has been introduced in a1562b30b2774acc7368ab69ab3dcea5b40613df

src/spi/cppthreads/CppThreadsTimer.cpp
@@ -67,7 +67,7 @@ bool CppThreadsTimer::isRunning() {
 void CppThreadsTimer::routine()
 {
 	std::unique_lock<std::mutex> lock(this->cv_m);
-	this->cv.wait_for(lock, std::chrono::milliseconds(this->duration));
+	this->cv.wait_for(lock, std::chrono::milliseconds(this->duration), [this]{return !this->started;});
 	if (this->started) {
 		this->callback(this);
 	}