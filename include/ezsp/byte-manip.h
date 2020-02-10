Legrandgroup/libezsp

commit ca299ac98cc6577db7ad01908108fa5d589e38ce

Using more bit manipulation utility functions

include/ezsp/byte-manip.h
@@ -30,4 +30,40 @@ inline uint8_t u16_get_lo_u8(const uint16_t word) {
 	return static_cast<uint8_t>(word & 0xFF);
 }
 
+/**
+ * @brief Get bits 7 to 0 of a 32-bit value
+ *
+ * @return The result as a byte (uint8_t)
+**/
+inline uint8_t u32_get_byte0(const uint32_t word32) {
+	return static_cast<uint8_t>(word32 & 0xFF);
+}
+
+/**
+ * @brief Get bits 15 to 8 of a 32-bit value
+ *
+ * @return The result as a byte (uint8_t)
+**/
+inline uint8_t u32_get_byte1(const uint16_t word32) {
+	return static_cast<uint8_t>((word32>>8) & 0xFF);
+}
+
+/**
+ * @brief Get bits 23 to 16 of a 32-bit value
+ *
+ * @return The result as a byte (uint8_t)
+**/
+inline uint8_t u32_get_byte2(const uint16_t word32) {
+	return static_cast<uint8_t>((word32>>16) & 0xFF);
+}
+
+/**
+ * @brief Get bits 31 to 24 of a 32-bit value
+ *
+ * @return The result as a byte (uint8_t)
+**/
+inline uint8_t u32_get_byte3(const uint16_t word32) {
+	return static_cast<uint8_t>((word32>>24) & 0xFF);
+}
+
 } // namespace NSEZSP

src/ezsp/ezsp-protocol/struct/ember-gp-address-struct.cpp
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <iomanip>
 
+#include "ezsp/byte-manip.h"
 #include "ember-gp-address-struct.h"
 
 using NSEZSP::CEmberGpAddressStruct;
@@ -25,19 +26,18 @@ CEmberGpAddressStruct::CEmberGpAddressStruct(const std::vector<uint8_t>& raw_mes
 }
 
 CEmberGpAddressStruct::CEmberGpAddressStruct(const uint32_t i_srcId):
-	gpdIeeeAddress(),	/* FIXME */
+	// IEEE address will contain the source ID duplicated twice
+	gpdIeeeAddress({ u32_get_byte0(i_srcId),
+	                 u32_get_byte1(i_srcId),
+	                 u32_get_byte2(i_srcId),
+	                 u32_get_byte3(i_srcId),
+	                 u32_get_byte0(i_srcId),
+	                 u32_get_byte1(i_srcId),
+	                 u32_get_byte2(i_srcId),
+	                 u32_get_byte3(i_srcId) }),
 	applicationId(0),
 	endpoint(0)
 {
-    // update Ieee with twice SourceId
-    gpdIeeeAddress.push_back(static_cast<uint8_t>(i_srcId&0xFF));
-    gpdIeeeAddress.push_back(static_cast<uint8_t>((i_srcId>>8)&0xFF));
-    gpdIeeeAddress.push_back(static_cast<uint8_t>((i_srcId>>16)&0xFF));
-    gpdIeeeAddress.push_back(static_cast<uint8_t>((i_srcId>>24)&0xFF));
-    gpdIeeeAddress.push_back(static_cast<uint8_t>(i_srcId&0xFF));
-    gpdIeeeAddress.push_back(static_cast<uint8_t>((i_srcId>>8)&0xFF));
-    gpdIeeeAddress.push_back(static_cast<uint8_t>((i_srcId>>16)&0xFF));
-    gpdIeeeAddress.push_back(static_cast<uint8_t>((i_srcId>>24)&0xFF));
 }
 
 std::vector<uint8_t> CEmberGpAddressStruct::getRaw() const

src/ezsp/ezsp-protocol/struct/ember-gp-sink-table-entry-struct.cpp
@@ -8,7 +8,6 @@
 #include <iomanip>
 
 #include "ezsp/ezsp-protocol/struct/ember-gp-sink-table-entry-struct.h"
-
 #include "ezsp/byte-manip.h"
 
 using NSEZSP::CEmberGpSinkTableEntryStruct;
@@ -36,7 +35,7 @@ CEmberGpSinkTableEntryStruct::~CEmberGpSinkTableEntryStruct()
 CEmberGpSinkTableEntryStruct::CEmberGpSinkTableEntryStruct(const std::vector<uint8_t>& raw_message):
         status(raw_message.at(0)),
         options(dble_u8_to_u16(raw_message.at(2),raw_message.at(1))),
-        gpd(std::vector<uint8_t>(raw_message.begin()+3,raw_message.end())),
+        gpd(NSSPI::ByteBuffer(raw_message.begin()+3,raw_message.end())),
         device_id(raw_message.at(13)),
         sink_list(),
         assigned_alias(dble_u8_to_u16(raw_message.at(37),raw_message.at(36))),
@@ -94,10 +93,10 @@ std::vector<uint8_t> CEmberGpSinkTableEntryStruct::getRaw() const
     // The security options field.
     l_struct.push_back(security_options);
     // The security frame counter of the GPD.
-    l_struct.push_back(static_cast<uint8_t>(gpdSecurity_frame_counter&0xFF));
-    l_struct.push_back(static_cast<uint8_t>((gpdSecurity_frame_counter>>8)&0xFF));
-    l_struct.push_back(static_cast<uint8_t>((gpdSecurity_frame_counter>>16)&0xFF));
-    l_struct.push_back(static_cast<uint8_t>((gpdSecurity_frame_counter>>24)&0xFF));
+    l_struct.push_back(u32_get_byte0(gpdSecurity_frame_counter));
+    l_struct.push_back(u32_get_byte1(gpdSecurity_frame_counter));
+    l_struct.push_back(u32_get_byte2(gpdSecurity_frame_counter));
+    l_struct.push_back(u32_get_byte3(gpdSecurity_frame_counter));
 
     // The key to use for GPD.
     l_struct.insert(l_struct.end(), gpd_key.begin(), gpd_key.end());

src/ezsp/ezsp-protocol/struct/ember-network-parameters.cpp
@@ -8,7 +8,6 @@
 #include <iomanip>
 
 #include "ezsp/ezsp-protocol/struct/ember-network-parameters.h"
-
 #include "ezsp/byte-manip.h"
 
 using NSEZSP::CEmberNetworkParameters;
@@ -41,11 +40,7 @@ CEmberNetworkParameters::CEmberNetworkParameters(const NSSPI::ByteBuffer& raw_me
 	join_method(static_cast<EmberJoinMethod>(raw_message.at(12+skip))),
 	nwk_manager_id(static_cast<EmberNodeId>(dble_u8_to_u16(raw_message.at(14+skip), raw_message.at(13+skip)))),
 	nwk_update_id(raw_message.at(15+skip)),
-	channels(
-		static_cast<uint32_t>(raw_message.at(16)) |
-		static_cast<uint32_t>(raw_message.at(17))<<8 |
-		static_cast<uint32_t>(raw_message.at(18))<<16 |
-		static_cast<uint32_t>(raw_message.at(19))<<24)
+	channels(quad_u8_to_u32(raw_message.at(19), raw_message.at(18), raw_message.at(17), raw_message.at(16)))
 {
 }
 
@@ -78,17 +73,17 @@ NSSPI::ByteBuffer CEmberNetworkParameters::getRaw() const
     raw_message.push_back(static_cast<uint8_t>(join_method));
 
     // nwk_manager_id
-    raw_message.push_back(static_cast<uint8_t>(nwk_manager_id&0xFF));
-    raw_message.push_back(static_cast<uint8_t>(static_cast<uint8_t>(nwk_manager_id>>8)&0xFF));
+    raw_message.push_back(u16_get_lo_u8(static_cast<uint16_t>(nwk_manager_id)));
+    raw_message.push_back(u16_get_hi_u8(static_cast<uint16_t>(nwk_manager_id)));
 
     // nwk_update_id
     raw_message.push_back(static_cast<uint8_t>(nwk_update_id));
 
     // channels
-    raw_message.push_back(static_cast<uint8_t>(channels&0xFF));
-    raw_message.push_back(static_cast<uint8_t>((channels>>8)&0xFF));
-    raw_message.push_back(static_cast<uint8_t>((channels>>16)&0xFF));
-    raw_message.push_back(static_cast<uint8_t>((channels>>24)&0xFF));
+    raw_message.push_back(u32_get_byte0(channels));
+    raw_message.push_back(u32_get_byte1(channels));
+    raw_message.push_back(u32_get_byte2(channels));
+    raw_message.push_back(u32_get_byte3(channels));
 
     return raw_message;
 }

src/ezsp/ezsp-protocol/struct/ember-process-gp-pairing-parameter.cpp
@@ -6,6 +6,7 @@
  * Reference: docs-14-0563-16-batt-green-power-spec_ProxyBasic.pdf
  */
 
+#include "ezsp/byte-manip.h"
 #include "ember-process-gp-pairing-parameter.h"
 
 using NSEZSP::CProcessGpPairingParam;
@@ -49,33 +50,33 @@ std::vector<uint8_t> CProcessGpPairingParam::get() const
 
     // The options field of the GP Pairing command
     uint32_t l_option = options.get();
-    lo_out.push_back(static_cast<uint8_t>(l_option&0xFF));
-    lo_out.push_back(static_cast<uint8_t>((l_option>>8)&0xFF));
-    lo_out.push_back(static_cast<uint8_t>((l_option>>16)&0xFF));
-    lo_out.push_back(static_cast<uint8_t>((l_option>>24)&0xFF));
+    lo_out.push_back(u32_get_byte0(l_option));
+    lo_out.push_back(u32_get_byte1(l_option));
+    lo_out.push_back(u32_get_byte2(l_option));
+    lo_out.push_back(u32_get_byte3(l_option));
     // The addressing info of the target GPD.
     std::vector<uint8_t> l_gpd_addr = addr.getRaw();
     lo_out.insert(lo_out.end(),l_gpd_addr.begin(),l_gpd_addr.end());
     // The communication mode of the GP Sink.
     lo_out.push_back(commMode);
     // The network address of the GP Sink.
-    lo_out.push_back(static_cast<uint8_t>(sinkNetworkAddress&0xFF));
-    lo_out.push_back(static_cast<uint8_t>(static_cast<uint8_t>(sinkNetworkAddress>>8)&0xFF));
+    lo_out.push_back(u16_get_lo_u8(sinkNetworkAddress));
+    lo_out.push_back(u16_get_hi_u8(sinkNetworkAddress));
     // The group ID of the GP Sink.
-    lo_out.push_back(static_cast<uint8_t>(sinkGroupId&0xFF));
-    lo_out.push_back(static_cast<uint8_t>(static_cast<uint8_t>(sinkGroupId>>8)&0xFF));
+    lo_out.push_back(u16_get_lo_u8(sinkGroupId));
+    lo_out.push_back(u16_get_hi_u8(sinkGroupId));
     // The alias assigned to the GPD.
-    lo_out.push_back(static_cast<uint8_t>(assignedAlias&0xFF));
-    lo_out.push_back(static_cast<uint8_t>(static_cast<uint8_t>(assignedAlias>>8)&0xFF));
+    lo_out.push_back(u16_get_lo_u8(assignedAlias));
+    lo_out.push_back(u16_get_hi_u8(assignedAlias));
     // The IEEE address of the GP Sink.
     lo_out.insert(lo_out.end(),sinkIeeeAddress.begin(),sinkIeeeAddress.end());
     // The key to use for GPD.
     lo_out.insert(lo_out.end(),gpdKey.begin(),gpdKey.end());
     // The security frame counter of the GPD.
-    lo_out.push_back(static_cast<uint8_t>(gpdSecurityFrameCounter&0xFF));
-    lo_out.push_back(static_cast<uint8_t>(static_cast<uint8_t>(gpdSecurityFrameCounter>>8)&0xFF));
-    lo_out.push_back(static_cast<uint8_t>(static_cast<uint8_t>(gpdSecurityFrameCounter>>16)&0xFF));
-    lo_out.push_back(static_cast<uint8_t>(static_cast<uint8_t>(gpdSecurityFrameCounter>>24)&0xFF));
+    lo_out.push_back(u32_get_byte0(gpdSecurityFrameCounter));
+    lo_out.push_back(u32_get_byte1(gpdSecurityFrameCounter));
+    lo_out.push_back(u32_get_byte2(gpdSecurityFrameCounter));
+    lo_out.push_back(u32_get_byte3(gpdSecurityFrameCounter));
     // The forwarding radius.
     lo_out.push_back(forwardingRadius);
 

src/ezsp/zigbee-tools/green-power-sink.cpp
@@ -690,8 +690,8 @@ void CGpSink::gpBrCommissioningNotification( uint32_t i_gpd_src_id, uint8_t i_se
     std::vector<uint8_t> l_proxy_br_payload;
 
     // The source from which to send the broadcast
-    l_proxy_br_payload.push_back(static_cast<uint8_t>(i_gpd_src_id&0xFF));
-    l_proxy_br_payload.push_back(static_cast<uint8_t>((i_gpd_src_id>>8)&0xFF));
+    l_proxy_br_payload.push_back(u16_get_lo_u8(i_gpd_src_id));
+    l_proxy_br_payload.push_back(u16_get_hi_u8(i_gpd_src_id));
 
     // The destination to which to send the broadcast. This must be one of the three ZigBee broadcast addresses.
     l_proxy_br_payload.push_back(0xFD);
@@ -780,10 +780,10 @@ void CAppDemo::ezspGetExtendedValue( uint8_t i_value_id, uint32_t i_characterist
     l_payload.push_back(i_value_id);
 
     // Identifies which characteristics of the extended value ID to read. These are specific to the value being read.
-    l_payload.push_back((uint8_t)(i_characteristic&0xFF));
-    l_payload.push_back((uint8_t)((i_characteristic>>8)&0xFF));
-    l_payload.push_back((uint8_t)((i_characteristic>>16)&0xFF));
-    l_payload.push_back((uint8_t)((i_characteristic>>24)&0xFF));
+    l_payload.push_back(u32_get_byte0(i_characteristic));
+    l_payload.push_back(u32_get_byte1(i_characteristic));
+    l_payload.push_back(u32_get_byte2(i_characteristic));
+    l_payload.push_back(u32_get_byte3(i_characteristic));
 
     clogI << "EZSP_GET_EXTENDED_VALUE\n";
     dongle.sendCommand(EZSP_GET_EXTENDED_VALUE, l_payload);
@@ -797,14 +797,14 @@ void CAppDemo::gpSinkTableLookup( uint32_t i_src_id )
 
     // EmberGpAddress addr The address to search for.
     l_payload.push_back(0x00);
-    l_payload.push_back((uint8_t)(i_src_id&0xFF));
-    l_payload.push_back((uint8_t)((i_src_id>>8)&0xFF));
-    l_payload.push_back((uint8_t)((i_src_id>>16)&0xFF));
-    l_payload.push_back((uint8_t)((i_src_id>>24)&0xFF));
-    l_payload.push_back((uint8_t)(i_src_id&0xFF));
-    l_payload.push_back((uint8_t)((i_src_id>>8)&0xFF));
-    l_payload.push_back((uint8_t)((i_src_id>>16)&0xFF));
-    l_payload.push_back((uint8_t)((i_src_id>>24)&0xFF));
+    l_payload.push_back(u32_get_byte0(i_src_id));
+    l_payload.push_back(u32_get_byte1(i_src_id));
+    l_payload.push_back(u32_get_byte2(i_src_id));
+    l_payload.push_back(u32_get_byte3(i_src_id));
+    l_payload.push_back(u32_get_byte0(i_src_id));
+    l_payload.push_back(u32_get_byte1(i_src_id));
+    l_payload.push_back(u32_get_byte2(i_src_id));
+    l_payload.push_back(u32_get_byte3(i_src_id));
     l_payload.push_back(0x00);
 
     clogI << "EZSP_GP_SINK_TABLE_LOOKUP\n";

src/ezsp/zigbee-tools/zigbee-networking.cpp
@@ -200,8 +200,8 @@ void CZigbeeNetworking::formHaNetwork(uint8_t channel)
 
     NSSPI::ByteBuffer payload;
     // security bitmask
-    payload.push_back(static_cast<uint8_t>(l_security_bitmak&0xFF));
-    payload.push_back(static_cast<uint8_t>(static_cast<uint8_t>(l_security_bitmak>>8)&0xFF));
+    payload.push_back(u16_get_lo_u8(l_security_bitmak));
+    payload.push_back(u16_get_hi_u8(l_security_bitmak));
     // tc key : HA Key
     payload.push_back(0x5A);
     payload.push_back(0x69);

commit f011278c1cbd54c85dbad843e12c8e717e9b6ccd

Fixing uint32_t dissector bug introduced in commit ca299ac98cc6577db7ad01908108fa5d589e38ce

include/ezsp/byte-manip.h
@@ -74,7 +74,7 @@ inline uint8_t u32_get_byte0(const uint32_t word32) {
  * @param word32 The 32-bit input value
  * @return The result as a byte (uint8_t)
 **/
-inline uint8_t u32_get_byte1(const uint16_t word32) {
+inline uint8_t u32_get_byte1(const uint32_t word32) {
 	return static_cast<uint8_t>((word32>>8) & 0xFF);
 }
 
@@ -84,7 +84,7 @@ inline uint8_t u32_get_byte1(const uint16_t word32) {
  * @param word32 The 32-bit input value
  * @return The result as a byte (uint8_t)
 **/
-inline uint8_t u32_get_byte2(const uint16_t word32) {
+inline uint8_t u32_get_byte2(const uint32_t word32) {
 	return static_cast<uint8_t>((word32>>16) & 0xFF);
 }
 
@@ -94,7 +94,7 @@ inline uint8_t u32_get_byte2(const uint16_t word32) {
  * @param word32 The 32-bit input value
  * @return The result as a byte (uint8_t)
 **/
-inline uint8_t u32_get_byte3(const uint16_t word32) {
+inline uint8_t u32_get_byte3(const uint32_t word32) {
 	return static_cast<uint8_t>((word32>>24) & 0xFF);
 }
 