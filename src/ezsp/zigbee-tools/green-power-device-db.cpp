12df6e23cc552790d2c819a81e81c501a0061780
Lionel AINS <lionel.ains@legrand.fr>
2020-03-05 10:16:30 +0100

    Running astyle on new green-power-device-db.{cpp,h} files

--- src/ezsp/zigbee-tools/green-power-device-db.cpp
+++ src/ezsp/zigbee-tools/green-power-device-db.cpp
@@ -12,43 +12,37 @@
 using NSEZSP::CGPDeviceDb;
 
 CGPDeviceDb::CGPDeviceDb() :
-    gp_dev_list()
-{
+	gp_dev_list() {
 }
 
-void CGPDeviceDb::clear()
-{
-    this->gp_dev_list.clear();
+void CGPDeviceDb::clear() {
+	this->gp_dev_list.clear();
 }
 
-void CGPDeviceDb::insertDevice(uint32_t i_source_id, const NSEZSP::EmberKeyData& i_key)
-{
-    if (this->gp_dev_list.insert(std::make_pair(i_source_id, i_key)).second == false) {
-        clogW << "Overwriting source ID 0x" << std::hex << std::setw(4) << std::setfill('0') << i_source_id << "\n";
-    }
+void CGPDeviceDb::insertDevice(uint32_t i_source_id, const NSEZSP::EmberKeyData& i_key) {
+	if (this->gp_dev_list.insert(std::make_pair(i_source_id, i_key)).second == false) {
+		clogW << "Overwriting source ID 0x" << std::hex << std::setw(4) << std::setfill('0') << i_source_id << "\n";
+	}
 }
 
-void CGPDeviceDb::insertDevice(const NSEZSP::CGpDevice& i_gp_device)
-{
-    this->insertDevice(i_gp_device.getSourceId(), i_gp_device.getKey());
+void CGPDeviceDb::insertDevice(const NSEZSP::CGpDevice& i_gp_device) {
+	this->insertDevice(i_gp_device.getSourceId(), i_gp_device.getKey());
 }
 
-void CGPDeviceDb::setDb(const std::vector<NSEZSP::CGpDevice>& i_gp_devices_list)
-{
-    this->gp_dev_list.clear();
-    for(auto it = i_gp_devices_list.begin(); it != i_gp_devices_list.end(); ++it) {
-        this->insertDevice(*it);
-    }
+void CGPDeviceDb::setDb(const std::vector<NSEZSP::CGpDevice>& i_gp_devices_list) {
+	this->gp_dev_list.clear();
+	for(auto it = i_gp_devices_list.begin(); it != i_gp_devices_list.end(); ++it) {
+		this->insertDevice(*it);
+	}
 }
 
-bool CGPDeviceDb::getKeyForSourceId(uint32_t i_source_id, NSEZSP::EmberKeyData& o_key) const
-{
-    auto search = this->gp_dev_list.find(i_source_id);
-    if (search != this->gp_dev_list.end()) {
-        o_key = search->second;
-        return true;
-    }
-    else {
-        return false;
-    }
+bool CGPDeviceDb::getKeyForSourceId(uint32_t i_source_id, NSEZSP::EmberKeyData& o_key) const {
+	auto search = this->gp_dev_list.find(i_source_id);
+	if (search != this->gp_dev_list.end()) {
+		o_key = search->second;
+		return true;
+	}
+	else {
+		return false;
+	}
 }
--- src/ezsp/zigbee-tools/green-power-device-db.h
+++ src/ezsp/zigbee-tools/green-power-device-db.h
@@ -12,56 +12,55 @@
 
 namespace NSEZSP {
 
-class CGPDeviceDb
-{
+class CGPDeviceDb {
 public:
-    CGPDeviceDb();
+	CGPDeviceDb();
 
-    /**
-     * @brief Clear all entries in the database
-     */
-    void clear();
+	/**
+	 * @brief Clear all entries in the database
+	 */
+	void clear();
 
-    /**
-     * @brief Add/overwrite a device entry in the database
-     * 
-     * @note If an element with the same source ID already exist, it will be overwritten
-     * 
-     * @param[in] i_source_id The source ID of the new device to add
-     * @param[in] i_key The key of the new device to add
-     */
-    void insertDevice(uint32_t i_source_id, const NSEZSP::EmberKeyData& i_key);
+	/**
+	 * @brief Add/overwrite a device entry in the database
+	 *
+	 * @note If an element with the same source ID already exist, it will be overwritten
+	 *
+	 * @param[in] i_source_id The source ID of the new device to add
+	 * @param[in] i_key The key of the new device to add
+	 */
+	void insertDevice(uint32_t i_source_id, const NSEZSP::EmberKeyData& i_key);
 
-    /**
-     * @brief Add/overwrite a device entry in the database
-     * 
-     * @note If an element with the same source ID already exist, it will be overwritten
-     * 
-     * @param[in] i_gp_device A CGpDevice object containing the source ID and the key of the new device to add
-     */
-    void insertDevice(const NSEZSP::CGpDevice& i_gp_device);
+	/**
+	 * @brief Add/overwrite a device entry in the database
+	 *
+	 * @note If an element with the same source ID already exist, it will be overwritten
+	 *
+	 * @param[in] i_gp_device A CGpDevice object containing the source ID and the key of the new device to add
+	 */
+	void insertDevice(const NSEZSP::CGpDevice& i_gp_device);
 
-    /**
-     * @brief Set the database with the exact content provided as specific device list
-     * 
-     * @note The database will contain exactly this device list, any previous content will be destroyed
-     * 
-     * @param[in] i_gp_devices_list A vector of CGpDevice objects containing the source ID and the key of all devices to store in the database
-     */
-    void setDb(const std::vector<NSEZSP::CGpDevice>& i_gp_devices_list);
+	/**
+	 * @brief Set the database with the exact content provided as specific device list
+	 *
+	 * @note The database will contain exactly this device list, any previous content will be destroyed
+	 *
+	 * @param[in] i_gp_devices_list A vector of CGpDevice objects containing the source ID and the key of all devices to store in the database
+	 */
+	void setDb(const std::vector<NSEZSP::CGpDevice>& i_gp_devices_list);
 
-    /**
-     * @brief Retrieve the key for a specific source ID
-     * 
-     * @param[in] i_source_id The source ID we are searching
-     * @param[out] o_key a variable where to store the key corresponding to @p o_source_id
-     * 
-     * @return true if the source ID could be found in the database, false otherwise (in that case, o_source_id will remain unchanged)
-     */
-    bool getKeyForSourceId(uint32_t i_source_id, NSEZSP::EmberKeyData& o_key) const;
+	/**
+	 * @brief Retrieve the key for a specific source ID
+	 *
+	 * @param[in] i_source_id The source ID we are searching
+	 * @param[out] o_key a variable where to store the key corresponding to @p o_source_id
+	 *
+	 * @return true if the source ID could be found in the database, false otherwise (in that case, o_source_id will remain unchanged)
+	 */
+	bool getKeyForSourceId(uint32_t i_source_id, NSEZSP::EmberKeyData& o_key) const;
 
 private:
-    std::map<uint32_t, NSEZSP::EmberKeyData> gp_dev_list;   /*!< The green power device list stored in this class */
+	std::map<uint32_t, NSEZSP::EmberKeyData> gp_dev_list;   /*!< The green power device list stored in this class */
 };
 
 } // namespace NSEZSP
