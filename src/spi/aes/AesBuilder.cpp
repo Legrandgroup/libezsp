8e642a7bc27033ea3fa8428739d0b2dd5bf46cd1
Lionel AINS <lionel.ains@legrand.fr>
2020-03-05 15:15:23 +0100

    Adding duplicate or missing SPI detection in AesBuilder. Closes #47

--- src/spi/aes/AesBuilder.cpp
+++ src/spi/aes/AesBuilder.cpp
@@ -1,9 +1,15 @@
 #include "spi/aes/AesBuilder.h"
 
+#undef __AES_SPI_FOUND__
+#define __AES_SPI_FOUND__
 #ifdef USE_AESCUSTOM
 #include "spi/aes/custom/custom-aes.h"
 typedef NSSPI::CAes Aes;
 #endif
+#ifndef __AES_SPI_FOUND__
+# error At least one AES SPI should be selected
+#endif
+#undef __AES_SPI_FOUND__
 
 using NSSPI::AesBuilder;
 using NSSPI::IAes;
@@ -15,6 +21,6 @@ IAes *AesBuilder::create()
 
 	return &instance;
 #else //DYNAMIC_ALLOCATION
-  return new Aes();
+	return new Aes();
 #endif
 }
