ad1261cfda69536edc1585ff0fd2ee1db9b0bf11
Lionel AINS <lionel.ains@legrand.fr>
2020-03-18 07:57:46 +0100

    Disabling SonarCloud on one specific line in gp_tests.cpp

--- tests/gp_tests.cpp
+++ tests/gp_tests.cpp
@@ -156,7 +156,8 @@ TEST(gp_tests, gp_recv_sensor_measurement) {
 	auto wcb = [&serialProcessor](size_t& writtenCnt, const void* buf, size_t cnt, std::chrono::duration<double, std::milli> delta) -> int {
 		return serialProcessor.onWriteCallback(writtenCnt, buf, cnt, delta);
 	};
-	std::shared_ptr<MockUartDriver> mockUartDriverHandle(new MockUartDriver(wcb));
+	/* We are disabling analisys on the line below because analyser doesn't like dynamic allocations, even if then stored in a smart pointer container */
+	std::shared_ptr<MockUartDriver> mockUartDriverHandle(new MockUartDriver(wcb)); //NOSONAR
 	if (mockUartDriverHandle->open("/dev/ttyUSB0", 115200) != 0) {
 		FAILF("Failed opening mock serial port");
 	}
